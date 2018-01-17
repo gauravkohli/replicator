ec3630bde406d8cdf82e4de97c0604a4cedba8d3
Nemanja Djuric <nemanja.djuric@booking.com>
2018-01-17 10:51:34 +0100

    Add more tests for QueryInspector class

--- src/test/java/com/booking/replication/sql/QueryInspectorTest.java
+++ src/test/java/com/booking/replication/sql/QueryInspectorTest.java
@@ -3,6 +3,7 @@ package com.booking.replication.sql;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class QueryInspectorTest {
@@ -17,4 +18,65 @@ public class QueryInspectorTest {
         assertEquals("57E404AC:000000002A5B1D64:A4608F96",
                 QueryInspector.extractPseudoGTID("use `booking_meta`; drop view if exists `booking_meta`.`_pseudo_gtid_hint__asc:57E404AC:000000002A5B1D64:A4608F96`"));
     }
+
+    @Test
+    public void isDDLTemporaryTable() {
+
+        String querySql = "CREATE TEMPORARY TABLE core.my_tmp_table \n" +
+                "(INDEX my_index_name (tag, time), UNIQUE my_unique_index_name (order_number))\n" +
+                "SELECT * FROM core.my_big_table\n" +
+                "WHERE my_val = 1";
+
+        assertTrue(QueryInspector.isDDLTemporaryTable(querySql));
+
+    }
+
+    @Test
+    public void isNotDDLTemporaryTAble() {
+        String querySql = "CREATE TABLE core.my_tmp_table \n" +
+                "(INDEX my_index_name (tag, time), UNIQUE my_unique_index_name (order_number))\n" +
+                "SELECT * FROM core.my_big_table\n" +
+                "WHERE my_val = 1";
+
+        assertFalse(QueryInspector.isDDLTemporaryTable(querySql));
+
+    }
+
+    @Test
+    public void isDDLTemporaryTableWithComment(){
+
+        String querySQL = "/* Stuff to comment */ CREATE TEMPORARY TABLE core.my_tmp_table \n" +
+                "(INDEX my_index_name (tag, time), UNIQUE my_unique_index_name (order_number))\n" +
+                "SELECT * FROM core.my_big_table\n" +
+                "WHERE my_val = 1 ";
+
+        assertTrue(QueryInspector.isDDLTemporaryTable(querySQL));
+    }
+
+    @Test
+    public void isDDLDefiner() {
+        String querySQL = "CREATE DEFINER=user@example.com";
+
+        assertTrue(QueryInspector.isDDLDefiner(querySQL));
+    }
+
+    @Test
+    public void isDDLView() {
+
+        String querySQL = "/* Stuff to comment */CREATE VIEW core.my_tmp_table \n" +
+                "(INDEX my_index_name (tag, time), UNIQUE my_unique_index_name (order_number))\n" +
+                "SELECT * FROM core.my_big_table\n" +
+                "WHERE my_val = 1 ";
+
+        assertTrue(QueryInspector.isDDLView(querySQL));
+    }
+
+    @Test
+    public void isAnalyze() {
+
+        String querySQL = "ANALYZE TABLE Employee";
+
+        assertTrue(QueryInspector.isAnalyze(querySQL));
+    }
+
 }
